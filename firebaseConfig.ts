85f5d6e68ffbb0655fc4eb2057d679d23492f381
Amna Sohail <amna.sohail7866@gmail.com>
2025-07-20 03:06:31 -0400

    updated config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIE0QmZf2UN0edNOWjAO-9d9na-J3m_dA",
-    authDomain: "taskbridge7866.firebaseapp.com",
-    projectId: "taskbridge7866",
-    storageBucket: "taskbridge7866.appspot.com",
-    messagingSenderId: "1009730223785",
-    appId: "1:1009730223785:web:fc615e2fb708ef7b00f3b8"
+  apiKey: 
+  authDomain: 
+  storageBucket: 
+  messagingSenderId: 
+  appId:
 };
 
-  // Initialize Firebase
+
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
